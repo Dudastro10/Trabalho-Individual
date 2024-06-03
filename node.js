917f2b8a7847b3c35f39480f0babb912e06b987d
Dudastro10 <167258870+Dudastro10@users.noreply.github.com>
2024-06-02 22:25:42 -0300

    Delete node.js

--- node.js
+++
@@ -1,5 +0,0 @@
-node index.js
-
-curl http://localhost:3000/livros
-curl -X POST http://localhost:3000/livros/compra -H "Content-Type: application/json" -d '{"titulo": "Meditações"}'
-curl -X POST http://localhost:3000/livros -H "Content-Type: application/json" -d '{"titulo": "Novo Livro", "autor": "Novo Autor", "genero": "Aventura", "exemplares": 5, "imagem": "url-da-imagem"}'
